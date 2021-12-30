6ade92885eeb4882ce5bfc88d17519d7634206c3
Kimberly Aguilar Hermoso <kimaguilar2017@gmail.com>
2021-12-30 14:59:53 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 
+//!!CHANGE PORT TO FIND SERVER GOING TO USE for next project!!
 const PORT = process.env.PORT || 3001;
 
 const app = express();
